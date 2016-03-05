916edd961fc0b42df1182727f551cc9ab72ee39d
nicholasruggeri <ruggerinicholas@gmail.com>
2016-03-05 18:45:07 +0100

    Update 2016-03-05T17:45:04.045Z

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-!function(n){var i=function(){function n(){console.log("module 1 init")}return{init:n}}();i.init()}(window),function(n){var i=function(){function n(){console.log("module 2 init")}return{init:n}}();i.init()}(window);
+(function(exports) {
+
+    var module_1 = (function() {
+
+        function init() {
+            console.log('module 1 init')
+        }
+
+        return {
+            init: init
+        };
+    }());
+
+    module_1.init();
+
+}(window));
+(function(exports) {
+
+    var module_2 = (function() {
+
+        function init() {
+            console.log('module 2 init')
+        }
+
+        return {
+            init: init
+        };
+    }());
+
+    module_2.init();
+
+}(window));
