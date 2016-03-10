c8a904a0ca995dc2f1aae36d25da4f5bcb2f11e8
nicholasruggeri <ruggerinicholas@gmail.com>
2016-03-10 20:14:57 +0100

    update

--- _assets/js/mod_1.js
+++ _assets/js/mod_1.js
@@ -1,9 +1,43 @@
 (function(exports) {
 
-    var module_1 = (function() {
+
+    var videoResize = (function() {
+
+        var mediaAspect = mediaAspect || 16/9;
+        var windowW = $(window).width();
+        var windowH = $(window).height();
+        var windowAspect = windowW/windowH;
+
+        var $el =  $('.fullscreen-bg');
 
         function init() {
-            console.log('module 1 init')
+            _resize();
+            $(window).on('resize', function (argument) {
+                _resize();
+            })
+        }
+
+        function _resize() {
+
+            windowW = $(window).width();
+            windowH = $(window).height();
+            windowAspect = windowW/windowH;
+
+            if (windowAspect < mediaAspect) {
+                $el.css({
+                    width: windowH*mediaAspect,
+                    height: windowH,
+                    top: 0,
+                    left: -(windowH*mediaAspect-windowW)/2
+                });
+            } else {
+                $el.css({
+                    width: windowW,
+                    height: windowW/mediaAspect,
+                    top: -(windowW/mediaAspect-windowH)/2,
+                    left: 0
+                });
+            }
         }
 
         return {
@@ -11,6 +45,6 @@
         };
     }());
 
-    module_1.init();
+    videoResize.init();
 
 }(window));
