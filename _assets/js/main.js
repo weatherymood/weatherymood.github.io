51581ea10a7c391b616bd28ffff81b1fe16b327a
nicholasruggeri <ruggerinicholas@gmail.com>
2016-03-24 18:40:14 +0100

    update landing

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,12 +5,16 @@
     }, 0)
 
     $(window).on('load', function(){
-        PRELOADER.remove()
-        $('.chrome').addClass('loaded')
-        if (!Modernizr.touchevents){
-            $('.fullscreen-bg').append('<iframe class="fullscreen-bg__video" width="1280" height="720" src="https://www.youtube.com/embed/GquEnoqZAK0?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;loop=1" frameborder="0" allowfullscreen></iframe>')
-            MEDIARESIZE.init();
-        }
+        setTimeout(function() {
+            PRELOADER.remove()
+            //$('.chrome').addClass('loaded')
+            if (!Modernizr.touchevents){
+                setTimeout(function() {
+                    $('.fullscreen-bg').append('<iframe class="fullscreen-bg__video" width="1280" height="720" src="https://www.youtube.com/embed/GquEnoqZAK0?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;loop=1" frameborder="0" allowfullscreen></iframe>')
+                    MEDIARESIZE.init();
+                }, 500)
+            }
+        }, 0)
     })
 
 }(jQuery, window));
