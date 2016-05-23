95a3a63166f70d51f697efb61670264e20a60ed5
nicholasruggeri <ruggerinicholas@gmail.com>
2016-05-23 17:05:31 +0200

    remove video bg

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,20 +1,16 @@
 (function($, exports) {
-
     var list = [
         '<img src="https://source.unsplash.com/1600x900/?rain">',
         '<img src="https://source.unsplash.com/1600x900/?cloudy">',
         '<img src="https://source.unsplash.com/1600x900/?sunset">',
         '<img src="https://source.unsplash.com/1600x900/?clear,sky">'
     ]
-
     setTimeout(function () {
         $('.fullscreen-bg').append(list[Math.floor(Math.random()*list.length)])
         PRELOADER.show()
     }, 0)
-
     $(window).on('load', function(){
         PRELOADER.remove()
         MEDIARESIZE.init();
     })
-
 }(jQuery, window));
