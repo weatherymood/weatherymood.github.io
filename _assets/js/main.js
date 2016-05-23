79e60497390ec28c07a6fd87f228bd9f5bcdd9a1
nicholasruggeri <ruggerinicholas@gmail.com>
2016-05-23 16:42:25 +0200

    add bg img unsplash

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,20 +1,20 @@
 (function($, exports) {
 
+    var list = [
+        '<img src="https://source.unsplash.com/1600x900/?rain">',
+        '<img src="https://source.unsplash.com/1600x900/?cloudy">',
+        '<img src="https://source.unsplash.com/1600x900/?sunset">',
+        '<img src="https://source.unsplash.com/1600x900/?clear,sky">'
+    ]
+
     setTimeout(function () {
+        $('.fullscreen-bg').append(list[Math.floor(Math.random()*list.length)])
         PRELOADER.show()
     }, 0)
 
     $(window).on('load', function(){
-        setTimeout(function() {
-            PRELOADER.remove()
-            //$('.chrome').addClass('loaded')
-            if (!Modernizr.touchevents){
-                setTimeout(function() {
-                    $('.fullscreen-bg').append('<iframe class="fullscreen-bg__video" width="1280" height="720" src="https://www.youtube.com/embed/GquEnoqZAK0?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;loop=1" frameborder="0" allowfullscreen></iframe>')
-                    MEDIARESIZE.init();
-                }, 500)
-            }
-        }, 0)
+        PRELOADER.remove()
+        MEDIARESIZE.init();
     })
 
 }(jQuery, window));
