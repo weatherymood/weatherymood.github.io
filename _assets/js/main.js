34d4929fb1addc8debac3d7a460c8a63655414c6
nicholasruggeri <ruggerinicholas@gmail.com>
2016-07-06 11:11:03 +0200

    add g verification

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,16 +1,18 @@
 (function($, exports) {
     var list = [
-        '<img src="https://source.unsplash.com/1600x900/?rain">',
-        '<img src="https://source.unsplash.com/1600x900/?cloudy">',
-        '<img src="https://source.unsplash.com/1600x900/?sunset">',
-        '<img src="https://source.unsplash.com/1600x900/?clear,sky">'
+        'https://source.unsplash.com/1600x900/?rain',
+        'https://source.unsplash.com/1600x900/?cloudy',
+        'https://source.unsplash.com/1600x900/?sunset',
+        'https://source.unsplash.com/1600x900/?clear,sky'
     ]
     setTimeout(function () {
-        $('.fullscreen-bg').append(list[Math.floor(Math.random()*list.length)])
+        $('.fullscreen-bg').append(
+            '<div class="full-bg" style="background: url('+list[Math.floor(Math.random()*list.length)]+'); background-size: cover"></div>'
+        )
         PRELOADER.show()
     }, 0)
     $(window).on('load', function(){
         PRELOADER.remove()
-        MEDIARESIZE.init();
+        // MEDIARESIZE.init();
     })
 }(jQuery, window));
